08344021eb3f9d84f4fb0ae7880c19dd33eb845e
rain <angel@latent.dev>
2025-05-08 21:46:10 +0100

    Update nvidia driver

--- machines/linux-workstation/config.nix
+++ machines/linux-workstation/config.nix
@@ -52,11 +52,12 @@ in
     # HACK: https://github.com/NixOS/nixpkgs/issues/375730#issuecomment-2625157971 for 6.13 kernel
     # TODO: remove this and switch to stable once the card is a bit older
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.86.16"; # use new 570 drivers
-      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-      usePersistenced = false;
+      version = "570.144";
+      sha256_64bit = "sha256-wLjX7PLiC4N2dnS6uP7k0TI9xVWAJ02Ok0Y16JVfO+Y=";
+      sha256_aarch64 = "sha256-6kk2NLeKvG88QH7/YIrDXW4sgl324ddlAyTybvb0BP0=";
+      openSha256 = "sha256-PATw6u6JjybD2OodqbKrvKdkkCFQPMNPjrVYnAZhK/E=";
+      settingsSha256 = "sha256-VcCa3P/v3tDRzDgaY+hLrQSwswvNhsm93anmOhUymvM=";
+      persistencedSha256 = "sha256-hx4w4NkJ0kN7dkKDiSOsdJxj9+NZwRsZEuhqJ5Rq3nM=";
     };
     modesetting.enable = true;
     powerManagement.enable = true;
