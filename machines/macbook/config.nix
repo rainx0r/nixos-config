885a1c053f2a1c800e26a7782c5d205d083e66a0
rain <evan@latent.dev>
2025-11-25 15:29:58 +0000

    Update macbook config

--- machines/macbook/config.nix
+++ machines/macbook/config.nix
@@ -3,23 +3,9 @@
 {
   system.stateVersion = 5;
 
-  nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      keep-outputs = true
-      keep-derivations = true
-    '';
-  };
+  nix.enable = false;
 
-  nix.gc = {
-    automatic = true;
-    interval = {
-      Weekday = 0;
-      Hour = 0;
-      Minute = 0;
-    };
-    options = "--delete-older-than 30d";
-  };
+  networking.hostName = "macbook";
 
   programs.zsh.enable = true;
   environment.shells = with pkgs; [
--- users/rain/darwin.nix
+++ users/rain/darwin.nix
@@ -10,6 +10,19 @@
         KeyRepeat = 2; # 120, 90, 60, 30, 12, 6, 2
         InitialKeyRepeat = 15; # 120, 94, 68, 35, 25, 15
       };
+      dock.persistent-apps = [
+        { app = "/System/Cryptexes/App/System/Applications/Safari.app"; }
+        { app = "/Applications/Ghostty.app/"; }
+        { app = "/Applications/Zed.app/"; }
+        { app = "/Applications/Zotero.app/"; }
+        { app = "/Applications/Things3.app/"; }
+        { app = "/System/Applications/Calendar.app/"; }
+        { app = "/Applications/ChatGPT.app/"; }
+        { app = "/Applications/Linear.app/"; }
+        { app = "/Applications/Discord.app/"; }
+        { app = "/System/Applications/Mail.app/"; }
+        { app = "/System/Applications/Music.app/"; }
+      ];
     };
   };
 
@@ -21,40 +34,45 @@
     brews = [
       "docker"
       "mas"
+      "ghidra"
     ];
     masApps = {
       # Essentials
       "Things 3" = 904280696;
       "XCode" = 497799835;
       # Design
-      "Affinity Publisher 2" = 1606941598;
-      "Affinity Photo 2" = 1616822987;
-      "Affinity Designer 2" = 1616831348;
       "Sketch" = 1667260533;
       # Office
       "Microsoft Word" = 462054704;
       "Microsoft Excel" = 462058435;
       "OneDrive" = 823766827;
       "Numbers" = 409203825;
+      "Keynote" = 409183694;
+      "Pages" = 409183694;
       # Safari extensions
       "SponsorBlock for Safari" = 1573461917;
       "Userscripts" = 1463298887;
       "Wipr" = 1320666476;
       "Bitwarden" = 1352778147;
+      "Vimlike" = 1584519802;
       # Music
-      "forScore" = 363738376;
       "Logic Pro" = 634148309;
+      "forScore" = 363738376;
+      # Video
+      "Final Cut Pro" = 424389933;
       # Utils
       "DaisyDisk" = 411643860;
-      # "AppCleaner" = 1616505989; NOTE: Currently no longer on the App Store
+      "Flighty" = 1358823008;
+      "field-kit" = 1612653346;
+      "Windows App" = 1295203466;
     };
     casks = [
       "karabiner-elements"
       "iina"
+      "stolendata-mpv"
       "swiftformat-for-xcode"
       "unnaturalscrollwheels"
       "figma"
-      "betterdisplay"
       # these probably could get moved to the linux config
       # if I start using that as a desktop ever
       "syncplay"
@@ -64,11 +82,12 @@
       "ghostty"
       "mullvad-vpn"
       "linear-linear"
-      "ghidra"
-      "obsidian"
       "obs"
+      "obsidian"
       "notion"
       "zed"
+      "visual-studio-code"
+      "chatgpt"
     ];
   };
 
