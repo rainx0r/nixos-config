c55d2bff94704e05cba2fe89bd898f44ebad501a
rain <evan@latent.dev>
2026-02-16 09:48:46 +0000

    Fix Echo connectivity

--- machines/linux-workstation/config.nix
+++ machines/linux-workstation/config.nix
@@ -128,6 +128,13 @@ in
       PasswordAuthentication = false;
       PermitRootLogin = "no";
       AllowUsers = [ "rain" ];
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-256"
+        "hmac-sha2-512"
+      ];
     };
   };
   services.fail2ban.enable = true;
